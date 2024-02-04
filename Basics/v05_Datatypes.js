863c57f0b6526acc8b1b351590a0b47db61f8fe9
Khishamuddin Syed <111244923+officialkmsyed@users.noreply.github.com>
2024-02-04 10:04:22 +0000

    Discuss datatypes and config tip.txt

---
+++ Basics/v05_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version of JavaScript
+
+// alert(3 + 3);    we are using node.js not browser, this alert not work on node js
+
+
+let name="Syed"
+
+let age=21
+
+let isLoggedIn = false
+
+
+/* ------------Notes-------------------
+
+DataTypes
+
+1. number    - used when =>2 to Power 53
+2. biginit   - used when number is more than 2 to power 53
+3. string    - used when tex, used in ' &  " 
+4. boolean   - used when Yes or No, 0 or 1, True or False
+5. null      - Its also a standalone value in JavaScript, like, empty value
+6. undefined - its used when value is not defined like, let age;
+7. symbol    - its mostly used in React, unique things
+
+8. object    - When output of console.log(typeof null); is object
+
+*/
