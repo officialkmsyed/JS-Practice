41eda6034c4c9045a4a9fdbbc9b22886746c74bb
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-09 16:38:06 +0000

    completed v16

--- 02_Basics/v16_Objects.js
+++ 02_Basics/v16_Objects.js
@@ -5,13 +5,16 @@
 Object.create; //Constructor Method
 
 //object literals method
+
+const mySym = Symbol("key1") // declare symbol syntax
 const JsUser = {
     name: "Syed",
     age: 18,
     location: "Aurangabad",
     email: "xyz@gmail.com",
     "isLoggedIn" : false,
-    lastLoginDays: ["Monday, Tuesday, Wednesday"]
+    lastLoginDays: ["Monday, Tuesday, Wednesday"],
+    [mySym]: "mykey1"   //Symbol object declaration syntax
 };
 
 //To get the object values 
@@ -19,3 +22,16 @@ const JsUser = {
 console.log(JsUser.email);   //xxxxxxxxxxxx Bad way to access Object
 
 console.log(JsUser["name"]);  //this is the right method to access the Object
+console.log(JsUser[mySym]);  //Print Symbol object (without double quote)
+
+//How to change object value
+
+JsUser.name='Khishamuddin';
+Object.freeze(JsUser); //freeze  all jsUser
+
+
+JsUser.name='Syed1';
+JsUser.email='syed@gmial.com';
+console.log(JsUser);
+
+//
