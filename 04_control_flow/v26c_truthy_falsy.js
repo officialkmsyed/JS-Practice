28f0b68b58a014e0e6b58b4d5f80d5cbff3029ba
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-13 18:11:28 +0000

    fix - indentation

--- 04_control_flow/v26c_truthy_falsy.js
+++ 04_control_flow/v26c_truthy_falsy.js
@@ -7,5 +7,3 @@
 
 //Ternary Operator:
 // The conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
-
-
