035d58c07cbd8338eb5d1bbfeeb0fa74eee17805
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-13 18:11:03 +0000

    complete - v26c control flow practice file

---
+++ 04_control_flow/v26c_truthy_falsy.js
@@ -0,0 +1,11 @@
+//In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy. That is, all values are truthy except false, 0, -0, BigInt 0n, "", null, undefined, NaN, and document.all.
+
+//Truthy & Falsey or Falsy
+
+//Nullish Coalescing ??:
+//The nullish coalescing (??) operator: is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+
+//Ternary Operator:
+// The conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
+
+
