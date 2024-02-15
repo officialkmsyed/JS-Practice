376a5570dadd009989572aa6bb2da6ba067416b3
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-15 08:46:48 +0000

    update - v28

--- 05_iterations/v28_02_do_while.js
+++ 05_iterations/v28_02_do_while.js
@@ -1,5 +1,2 @@
 // Do while
 
-git config --global user.signingkey 58CE4C3E2C588F46CF56403579FF3AE1E8D759C7
-git config --global commit.gpgsign true
-gpg --status-fd=2 -bsau 58CE4C3E2C588F46CF56403579FF3AE1E8D759C7
