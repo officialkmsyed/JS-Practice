a0dcd74d8fb58d0a4c1432898cf42ab724dcba78
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-15 10:10:10 +0000

    complete - v28 practice file

--- 05_iterations/v28_02_do_while.js
+++ 05_iterations/v28_02_do_while.js
@@ -1,2 +1,19 @@
 // Do while
 
+myArray = ["Flash", "BatMan", "SuperMan"];
+arr = 0;
+while (arr < myArray.length) {
+  console.log(`${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+//-------------------------DO WHILE-------------------------------------------
+
+//Do wile used when the program runs first and then condition will check
+
+// let score = 111; //Special Case in Do While 
+
+do {
+  console.log(`Score is ${score}`);
+  score++; //Increment Operator
+} while (score <= 10);
