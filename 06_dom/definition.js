658ab42536f5f6937aa3c9bd9e9e9193cd4771de
Khishamuddin Syed <official.kmsyed@gmail.com>
2024-02-18 10:30:31 +0000

    add - v31 dom practice file

---
+++ 06_dom/definition.js
@@ -0,0 +1,6 @@
+//The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
+
+// console.log(window);
+// console.dir(document);
