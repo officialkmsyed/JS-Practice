57ef15dcda48e40dbcd2209f913825d7f4bd285d
Khishamuddin Syed <kmsyed2003@gmail.com>
2024-02-23 09:25:13 +0530

    Delete 07_projecst directory

--- 07_projecst/script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to window's load event
-window.addEventListener('load', function () {
-  // Add loaded class to body to hide loader and show content
-  document.body.classList.add('loaded');
-});
